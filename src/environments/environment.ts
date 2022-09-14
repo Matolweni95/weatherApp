446b1e6709b682418f7d633a8066c51e00def1be
= <masixolematolweni.1@gmail.com>
2022-09-14 19:55:53 +0200

    testing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apikey: '',
+  apikey: 'masixole',
   BaseUrl: 'http://dataservice.accuweather.com/locations/v1/cities/search',
   forcastUrl: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/'
 
