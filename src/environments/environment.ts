d41492e7aa074d1d1c002cb0c35b5e7c2b6721fc
Masixole Matolweni <72172920+Matolweni95@users.noreply.github.com>
2022-09-14 19:34:20 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apikey: '5Di1F0kSm7kKt66wHUyl78jGod6lIsPn',
+  apikey: '',
   BaseUrl: 'http://dataservice.accuweather.com/locations/v1/cities/search',
   forcastUrl: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/'
 
